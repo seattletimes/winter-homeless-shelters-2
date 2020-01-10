d0d5c79d8371b47bd90b41fd16a1e9a318bbfe48
llflannery <lflannery@seattletimes.com>
2020-01-09 17:50:05 -0800

    runs

---
+++ src/js/main.js
@@ -0,0 +1,46 @@
+//load our custom elements
+require("component-leaflet-map");
+require("component-responsive-frame");
+
+//get access to Leaflet and the map
+var element = document.querySelector("leaflet-map");
+var L = element.leaflet;
+var map = element.map;
+
+// console.log('hi mom');
+
+//ICH code for popup template if needed----------
+// var ich = require("icanhaz");
+// var templateFile = require("./_popup.html");
+// ich.addTemplate("popup", templateFile);
+
+// var onEachFeature = function(feature, layer) {
+//   layer.bindPopup(ich.popup(feature.properties))
+// };
+var markers = [];
+var markergroup = L.featureGroup()
+
+window.course.forEach(function(data) {
+  var marker = L.marker([data.lat, data.lng], {
+    icon: L.divIcon({
+      className: "golf-icon"
+    })
+  });
+  var html = `
+    <b>${data.shelterName}</b><br>
+    <i>${data.city}</i><br>
+    <br>
+    Opened: ${data.openingDate}<br>
+    Shelter Type: ${data.shelterType}<br>
+
+  `;
+  console.log(marker);
+  marker.bindPopup(html);
+  markers.push(marker);
+  marker.addTo(markergroup);
+});
+
+markergroup.addTo(map);
+
+ map.scrollWheelZoom.disable();
+ map.fitBounds(markergroup.getBounds())
